11e154f7af521cfbaec1846c88148a9b3a834a15
ray.ksh <ray.ksh@kakaocorp.com>
2018-11-06 20:49:53 +0900

    footer 내용 추가

--- src/main/javascripts/components/FooterComponent.js
+++ src/main/javascripts/components/FooterComponent.js
@@ -1,17 +1,17 @@
 import React, {Component} from "react"
 
 
-class FooterComponent extends Component{
-
-    render(){
+class FooterComponent extends Component {
+    render() {
         return (
             <footer className={"footer"}>
-
+                <div>
+                    <p>Copyright 2018. <a href={"https://github.com/myks790"}>myks790</a></p>
+                    <p>이 페이지의 소스코드는 <a href={"https://github.com/myks790/MyHome"}>이 곳</a>에서 볼 수 있습니다.</p>
+                </div>
             </footer>)
     }
-
 }
-
 
 export default FooterComponent;
 
