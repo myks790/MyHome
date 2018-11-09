271ea768a4a862bb69ba67166a5e8250a8ad2cdf
Kang SangHun <myks790@gmail.com>
2018-11-09 14:37:18 +0900

    레포지토리 이름 변경에 따른 footer 링크 변경

--- src/main/javascripts/components/FooterComponent.js
+++ src/main/javascripts/components/FooterComponent.js
@@ -7,7 +7,7 @@ class FooterComponent extends Component {
             <footer className={"footer"}>
                 <div>
                     <p>Copyright 2018. <a href={"https://github.com/myks790"}>myks790</a></p>
-                    <p>이 페이지의 소스코드는 <a href={"https://github.com/myks790/MyHome"}>이 곳</a>에서 볼 수 있습니다.</p>
+                    <p>이 페이지의 소스코드는 <a href={"https://github.com/myks790/my-home"}>이 곳</a>에서 볼 수 있습니다.</p>
                 </div>
             </footer>)
     }
